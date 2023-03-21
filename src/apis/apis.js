6828f1919d5b38409778008c24d41b4797962c49
felipe veloza <ciberronaldo@hotmail.com>
2023-03-20 20:01:46 -0500

    Funcionalidad para conectar el api key de open.ai

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -2,19 +2,56 @@ import axios from "axios"
 
 export const validarAPIKey = async (APIKEY) => {
   try {
-    const response = await axios.post(
-      'https://api.openai.com/v1/engines/davinci/completions',
+    const response = await axios.post("https://api.openai.com/v1/chat/completions",
       {
-        prompt: 'hola chat gpt'
+        "model": "gpt-3.5-turbo",
+        "messages":
+        [
+          {
+            "role": "user",
+            "content": "Hola chat gpt, necesito que hagas una presentación super corta de que eres y a la ultimo digas (puedes usar el comando /promt <mensaje> para comenzar a hacer consultas)"
+          }
+        ]
       },
       {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${APIKEY}`,
+        "headers": {
+          "Authorization": `Bearer ${APIKEY}`,
+          "Content-Type": "application/json",
         },
       }
     );
-    return response.data.choices && response.data.choices[0] && response.data.choices[0].text ? true : false;
+
+    return response.data.choices[0].message.content
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export const sendPromptToGPT = async (prompt) => {
+  try {
+    const response = await axios.post("https://api.openai.com/v1/chat/completions",
+      {
+        "model": "gpt-3.5-turbo",
+        "messages":
+        [
+          {
+            "role": "user",
+            "content": prompt
+          }
+        ]
+      },
+      {
+        "headers": {
+          "Authorization": `Bearer ${APIKEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if(response.status !== 200){
+      return false
+    } return response.data.choices[0].message.content
+
   } catch (error) {
     console.error(error);
     return false;
--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import { Telegraf } from "telegraf"
-import { validarAPIKey } from "../apis/apis.js";
+import { validarAPIKey, sendPromptToGPT } from "../apis/apis.js";
 
 dotenv.config()
 const bot = new Telegraf(process.env.HTTP_ACCES_TG_TOKEN_BOT)
@@ -19,7 +19,7 @@ Instrucciones de uso: Para usarlo debes conectar tu apikey la cual obtienes de l
 https://platform.openai.com/account/api-keys
 
 para ello debes seguir los pasos:
-- iniciar sesión o crear una cuenta
+- crear una cuenta nueva en openAI (este paso es importante ya que la api de openai tiene un limite de consultas.)
 - ir a personal > view API keys > Create new secrete key.
 Una vez lista tu api key puedes usar el comando /apikey <YourApiKeyHere> | con este comando conectaras chatgpt con bot Crooks2k => IA-chatBot [Based on GPT-3]
 
@@ -30,19 +30,40 @@ bot.settings((ctx)=>{
 })
 
 //set userapikey
+let APIKEY
 bot.command('apikey', async (ctx) => {
-  const APIKEY = ctx.message.text.split(' ')[1];
+  APIKEY = ctx.message.text.split(' ')[1];
   if(!APIKEY){
     ctx.reply("digita un apikey valido (recuerda que debe existir un espacio entre el comando /apikey y el valor de tu apikey")
     return;
   }
   const isValid = await validarAPIKey(APIKEY);
   if(isValid){
-    ctx.reply("APIKEY conectada exitosamente")
+    // store the API key in the session
+    ctx.reply(`[GPT-3.5]: ${isValid}`)
     return;
   }else{
     ctx.reply("digita un apikey valido")
   }
+})
+
+//send prompt
+bot.command('prompt', async (ctx) => {
+  const prompt = ctx.message.text.split(' ')[1];
+  if(!prompt){
+    ctx.reply("digita un prompt valido (recuerda que debe existir un espacio entre el comando /prompt y tu consulta")
+    return;
+  }
+  if (!APIKEY) {
+    ctx.reply("Por favor ingresa un API key primero usando el comando /apikey")
+    return;
+  }
+  const response = await sendPromptToGPT(APIKEY, prompt);
+  if(response){
+    ctx.reply(`[GPT-3.5]: ${response}`)
+  }else(
+    ctx.reply("hubo un error al procesar tu respuesta, intenta mas tarde")
+  )
 })
 
 export {bot}
